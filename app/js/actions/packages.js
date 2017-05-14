604086f49c72aeec981016eebdf5d7b493e2ff16
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2017-05-14 10:09:34 +0530

    Update sass lint and actions

--- app/js/actions/packages.js
+++ app/js/actions/packages.js
@@ -27,34 +27,35 @@ const mapper = o => o.toJSON();
 const Package = Parse.Object.extend('Package');
 const Employee = Parse.Object.extend('Employee');
 
-export const fetchPackages = (searchToken = '') => (dispatch) => {
-  dispatch(requestPackages());
-  let query = new Parse.Query(Package);
-  if (searchToken.length > 3) {
-    const ownerNameQuery = new Parse.Query(Employee);
-    ownerNameQuery.contains('name', searchToken);
+export const fetchPackages = (searchToken = '') => (
+  (dispatch) => {
+    dispatch(requestPackages());
+    let query = new Parse.Query(Package);
+    if (searchToken.length > 3) {
+      const ownerNameQuery = new Parse.Query(Employee);
+      ownerNameQuery.contains('name', searchToken);
 
-    const ownerEmailQuery = new Parse.Query(Employee);
-    ownerEmailQuery.contains('email', searchToken);
+      const ownerEmailQuery = new Parse.Query(Employee);
+      ownerEmailQuery.contains('email', searchToken);
 
-    const ownerPhoneQuery = new Parse.Query(Employee);
-    ownerPhoneQuery.contains('phoneNumber', searchToken);
+      const ownerPhoneQuery = new Parse.Query(Employee);
+      ownerPhoneQuery.contains('phoneNumber', searchToken);
 
-    const compoundOwnerQuery = Parse.Query.or(ownerNameQuery, ownerEmailQuery, ownerPhoneQuery);
+      const compoundOwnerQuery = Parse.Query.or(ownerNameQuery, ownerEmailQuery, ownerPhoneQuery);
 
-    const packageIdQuery = new Parse.Query(Package);
-    packageIdQuery.contains('packageId', searchToken);
+      const packageIdQuery = new Parse.Query(Package);
+      packageIdQuery.contains('packageId', searchToken);
 
-    const ownerQuery = new Parse.Query(Package);
-    ownerQuery.matchesQuery('owner', compoundOwnerQuery);
+      const ownerQuery = new Parse.Query(Package);
+      ownerQuery.matchesQuery('owner', compoundOwnerQuery);
 
-    query = Parse.Query.or(ownerQuery, packageIdQuery);
+      query = Parse.Query.or(ownerQuery, packageIdQuery);
+    }
+    query.include('owner');
+    query.include('dealer');
+    query.find(qp).then((result) => {
+      const data = result.map(mapper);
+      dispatch(receivePackages(data));
+    });
   }
-  query.include('owner');
-  query.include('dealer');
-  query.find(qp).then((result) => {
-    const data = result.map(mapper);
-    dispatch(receivePackages(data));
-  });
-};
-
+);
