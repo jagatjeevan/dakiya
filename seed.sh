8cce7ce19d3d8b9d3c2e3b8624479f0666574446
MuhammedSameer Azazi <azazi-sa@github.com>
2017-08-11 11:29:51 +0530

    setup seed-data

--- seed.sh
+++ seed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Seeding _SCHEMA"
-mongoimport -d dakiya1 -c _SCHEMA --file db/_SCHEMA.json
+mongoimport -h db:27017 -d dakiya -c _SCHEMA --file seed-data/_SCHEMA.json
 
 echo "Seeding Employees"
-mongoimport -d dakiya1 -c Employee --file db/Employee.json
+mongoimport -h db:27017 -d dakiya -c Employee --file seed-data/Employee.json
 
 echo "Seeding Vendors"
-mongoimport -d dakiya1 -c Vendor --file db/Vendor.json
+mongoimport -h db:27017 -d dakiya -c Vendor --file seed-data/Vendor.json
